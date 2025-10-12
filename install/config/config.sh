b13e339780473dcd81b5103575b7458f5989a02f
Aldo W. Chacon Contreras <aldochaconc@gmail.com>
2025-10-12 18:36:50 -0300

    install: update scripts to use consolidated config paths

--- install/config/config.sh
+++ install/config/config.sh
@@ -2,6 +2,7 @@
 mkdir -p ~/.config
 cp -R ~/.local/share/omarchy/config/* ~/.config/
 
-# Use zshrc and set as default shell
-cp ~/.local/share/omarchy/default/zshrc ~/.zshrc
+# Use zshrc and bashrc and set zsh as default shell
+cp ~/.local/share/omarchy/config/zshrc ~/.zshrc
+cp ~/.local/share/omarchy/config/bashrc ~/.bashrc
 sudo chsh -s "$(which zsh)" "$USER"
--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -3,8 +3,8 @@ if [[ -n ${OMARCHY_ONLINE_INSTALL:-} ]]; then
   sudo pacman -S --needed --noconfirm base-devel
 
   # Configure pacman
-  sudo cp -f ~/.local/share/omarchy/default/pacman/pacman.conf /etc/pacman.conf
-  sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist /etc/pacman.d/mirrorlist
+  sudo cp -f ~/.local/share/omarchy/config/pacman/pacman.conf /etc/pacman.conf
+  sudo cp -f ~/.local/share/omarchy/config/pacman/mirrorlist /etc/pacman.d/mirrorlist
 
   # Refresh all repos
   sudo pacman -Syu --noconfirm
