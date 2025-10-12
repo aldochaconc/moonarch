cd23167655ac110076d85d483741d2ac796988bd
Aldo W. Chacon Contreras <aldochaconc@gmail.com>
2025-10-12 19:01:47 -0300

    config: migrate theme system from ~/.config/omarchy/themes to ~/dotfiles/themes
    - Update install/config/theme.sh to use dotfiles theme structure
    - Migrate all bin/omarchy-theme-* scripts to use ~/dotfiles/themes paths
    - Update CSS imports in walker themes to reference dotfiles paths
    - Update hypr configs (autostart, hyprlock) to use dotfiles theme paths
    - Update waybar and swayosd CSS imports to use dotfiles structure
    - Fix kanagawa theme name typo (kanawaga -> kanagawa)
    - Add exclusion of 'current' directory from theme lists
    - Maintain same symlink-based theme switching functionality

--- install/config/theme.sh
+++ install/config/theme.sh
@@ -2,23 +2,21 @@
 sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-previous-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-previous-symbolic.svg
 sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-next-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-next-symbolic.svg
 
-# Setup theme links
-mkdir -p ~/.config/omarchy/themes
-for f in ~/.local/share/omarchy/themes/*; do ln -nfs "$f" ~/.config/omarchy/themes/; done
+# Setup dotfiles theme directory structure
+mkdir -p ~/dotfiles/themes/current
 
-# Set initial theme
-mkdir -p ~/.config/omarchy/current
-ln -snf ~/.config/omarchy/themes/kanawaga ~/.config/omarchy/current/theme
-ln -snf ~/.config/omarchy/current/theme/backgrounds/1-kanawaga.jpg ~/.config/omarchy/current/background
+# Set initial theme (kanagawa in dotfiles)
+ln -snf ~/dotfiles/themes/kanagawa ~/dotfiles/themes/current/theme
+ln -snf ~/dotfiles/themes/current/theme/backgrounds/1-kanagawa.jpg ~/dotfiles/themes/current/background
 
-# Set specific app links for current theme
-ln -snf ~/.config/omarchy/current/theme/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
+# Set specific app links for current theme (pointing to dotfiles)
+ln -snf ~/dotfiles/themes/current/theme/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
 
 mkdir -p ~/.config/btop/themes
-ln -snf ~/.config/omarchy/current/theme/btop.theme ~/.config/btop/themes/current.theme
+ln -snf ~/dotfiles/themes/current/theme/btop.theme ~/.config/btop/themes/current.theme
 
 mkdir -p ~/.config/mako
-ln -snf ~/.config/omarchy/current/theme/mako.ini ~/.config/mako/config
+ln -snf ~/dotfiles/themes/current/theme/mako.ini ~/.config/mako/config
 
 # Add managed policy directories for Chromium for theme changes
 sudo mkdir -p /etc/chromium/policies/managed
