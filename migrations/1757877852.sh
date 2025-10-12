daa4bab5d87638037206aac8135f65b2000b8394
Aldo W. Chacon Contreras <aldochaconc@gmail.com>
2025-10-12 06:06:10 -0300

    chore: remove migration scripts

--- migrations/1757877852.sh
+++
@@ -1,6 +0,0 @@
-echo "Switch select bindings to launch or focus mode"
-
-if [[ -f ~/.config/hypr/bindings.conf ]]; then
-  sed -i '/SUPER, M, Music, exec/ c\bindd = SUPER, M, Music, exec, omarchy-launch-or-focus spotify' ~/.config/hypr/bindings.conf
-  sed -i '/SUPER SHIFT, G, WhatsApp, exec/ c\bindd = SUPER SHIFT, G, WhatsApp, exec, omarchy-launch-or-focus-webapp WhatsApp "https://web.whatsapp.com/"' ~/.config/hypr/bindings.conf
-fi
